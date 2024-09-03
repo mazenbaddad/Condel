ede81c6e7dd6934d0cad854b3b5076013f874f1a
mazenbaddad <mazenbaddad@hotmail.com>
2024-09-04 01:57:57 +0300

    init commit

---
+++ Condel/CondelApp.swift
@@ -0,0 +1,17 @@
+//
+//  CondelApp.swift
+//  Condel
+//
+//  Created by mazen baddad on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct CondelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
